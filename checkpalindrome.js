2327b5aaa6a56ffc3f17524050863e6aeb8eb31c
Edwin Rivera <waehake@gmail.com>
2017-09-20 21:02:03 -0400

    added the almostincreasingsequence problem

--- checkpalindrome.js
+++ checkpalindrome.js
@@ -4,7 +4,7 @@ function palindrome(word) {
   //use the split() function to split the string into an array of substrings
   //use the reverse() function to reverse the word
   //use join to create a string from the array of substrings
-  return word === word.split("").reverse().join(""); //returns true or false
+  return word === word.split('').reverse().join(''); //returns true or false
 };
 
 //test cases
