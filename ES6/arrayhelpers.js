ce12d5dbad2e53edbeb0acb0ba5a56a972274f84
Edwin Rivera <waehake@gmail.com>
2017-09-26 22:35:15 -0400

    added more array helper function examples

--- ES6/arrayhelpers.js
+++ ES6/arrayhelpers.js
@@ -36,3 +36,59 @@ var valsLessThan20 = values.filter(lessthan20);
 console.log(valsLessThan20);
 // output
 // [1,5]
+
+// find - the first element that passes the test implemented by the provided function, which should return true or false
+
+var people = [
+  {name: 'Ed', age: 28 },
+  {name: 'Mike', age: 30},
+  {name: 'Edward', age: 19},
+  {name: 'Ann', age: 32}
+];
+
+function teenager(person) {
+  return people.age > 10 &&  people.age < 20;
+};
+
+var firstTeenager = people.find(teenager);
+console.log('The first teenager is ', firstTeenager.name);
+//output - The first teenager is Edward
+
+// every - checks if every element in the array passes the test implemented by the provided function, which should be true or false.
+
+function drinkingAge(person) {
+  return person.age >= 21;
+};
+
+var alcohol4All = people.every(drinkingAge);
+console.log('Alcohol for all? ', alcohol4All);
+//output : Alcohol for all? false
+
+
+// some - checks if any element of the array passes the test implemented by the provided function, which should return true or false.
+function alcohol(person) {
+  return person.age < 21;
+};
+
+var underage = people.some(alcohol);
+
+console.log('There are some underage - can everyone drink alcohol? ', underage);
+//output - There are some underage - can everyone drink alcohol? false
+
+// reduce - applies a function pass as the first parameter against an accumulator and each element in hte array ( left to right), thus reducing it to a single value. The inital value of the accumulator should be provided as the second parameter of the reduce function.
+var array  = [1, 2, 3, 4];
+
+function sum(acc, value) {
+  return acc + value;
+};
+
+function product(acc, value) {
+  return acc * value;
+};
+
+var sumOfArrayElements = array.reduce(sum, 0);
+var productOfArrayElements = array.reduce(product, 1);
+console.log('Sum of ', array, 'is', sumOfArrayElements);
+console.log('Product of ', array, 'is', productOfArrayElements);
+// Sum of [1,2,3,4] is 10
+// Product of [1,2,3,4] is 24
