a4d223d17e790269982866427cff65b9dd60204c
Edwin Rivera <waehake@gmail.com>
2017-09-26 21:31:50 -0400

    updated the scoping reference to include a const variable example

--- ES6/1-varscoping.js
+++ ES6/1-varscoping.js
@@ -19,3 +19,21 @@ if (age > 13) {
   let dogYears2 = age * 7;
   console.log(`you are ${dogYears2} dog years old`); //variable is only available in the block scope.
 }
+
+// let - const in ES6
+const key = 'abc'; //use const when you don't want to change the value
+let points = 123; //use let
+let winner = false;
+
+if (points > 40) {
+  let winner = true; //declared inside a block - the winner is not a reference to the other winner
+};
+
+//properties of a const cannot be reassigned aka you cannot change name or age
+const person = {
+  name: 'Edwin',
+  age: 28,
+};
+//instead you can change the value
+person.name = 'Ed'; //valid
+person.age = 29; //valid
